7f3f3f0690895a71d7802e867058926269c96004
Nelson Gomes <nelsonmpg@gmail.com>
2017-08-27 09:35:26 +0100

    no message

--- startcollector.js
+++ startcollector.js
@@ -2,8 +2,8 @@
 
 require('colors');
 var cp = require('child_process'),
-    Collector = require('./collector/collector/collector.js');
+    Collector = require('./lib/collector/collector.js');
 
 
-var collector = new Collector();
+var collector = new Collector("http://192.168.1.75:8080");
 collector.init();
