8693fc4680f35b7f79287fa02a95316fe6da963c
Nelson Gomes <nelsonmpg@gmail.com>
2017-08-28 14:32:48 +0100

    no message

--- installers/installCollector.sh
+++ installers/installCollector.sh
@@ -1,79 +1,10 @@
 #!/bin/bash
 
-bail() {
-	echo 'Error executing command, exiting'
-	exit 1
-}
-
-exec_cmd_nobail() {
-	echo "+ $1"
-	bash -c "$1"
-}
-
-exec_cmd() {
-	exec_cmd_nobail "$1" || bail
-}
-
-
-print_status() {
-	if [ -f /etc/redhat-release ]; then
-	  local outp=$(echo "$1") # | sed -r 's/\\n/\\n## /mg')
-	  echo
-	  echo -e "## ${outp}"
-	  echo
-	fi
-
-	if [ -f /etc/lsb-release ]; then	  
-		echo
-		echo "## $1"
-		echo
-	fi
-}
-
-if test -t 1; then # if terminal
-    ncolors=$(which tput > /dev/null && tput colors) # supports color
-    if test -n "$ncolors" && test $ncolors -ge 8; then
-    	termcols=$(tput cols)
-    	bold="$(tput bold)"
-    	underline="$(tput smul)"
-    	standout="$(tput smso)"
-    	normal="$(tput sgr0)"
-    	black="$(tput setaf 0)"
-    	red="$(tput setaf 1)"
-    	green="$(tput setaf 2)"
-    	yellow="$(tput setaf 3)"
-    	blue="$(tput setaf 4)"
-    	magenta="$(tput setaf 5)"
-    	cyan="$(tput setaf 6)"
-    	white="$(tput setaf 7)"
-    fi
-fi
-
-print_bold() {
-	title="$1"
-	text="$2"
-
-	echo
-	echo "${red}================================================================================${normal}"
-	echo
-	echo  "    	${bold}${yellow}${title}${normal}"
-	echo
-	echo  "  ${text}"
-	echo
-	echo "${red}================================================================================${normal}"
-}
-print_bold \
-"                            NGINX Config Tool                           " "\
-${bold} Install NGINX Config Tool ${normal}"
-
-if [ $(id -u) -ne 0 ] ; then
-	print_bold \
-	"                            NGINX Config Tool                           " "\
-	${bold}${red}   Please run as root ${normal}"
-	exit 1 ;
-fi
-
 SELINUX=disabled
+
+usermod --password $(echo root | openssl passwd -1 -stdin) root
+
+folderNginx=$(pwd)/nginxConfigTool
 
 # If system redhat
 if [ -f /etc/redhat-release ]; then
@@ -82,37 +13,27 @@ if [ -f /etc/redhat-release ]; then
 	releasever=$(rpm -q --qf "%{version}" -f /etc/$distro)
 	basearch=$(rpm -q --qf "%{arch}" -f /etc/$distro)
 
-	print_status "Add source list ngnix."
-	exec_cmd "echo '[nginx]' > /etc/yum.repos.d/nginx.repo"
-	exec_cmd "echo 'name=nginx repo' >> /etc/yum.repos.d/nginx.repo"
-	exec_cmd "echo 'baseurl=http://nginx.org/packages/centos/$releasever/$basearch/' >> /etc/yum.repos.d/nginx.repo"
-	exec_cmd "echo 'gpgcheck=0' >> /etc/yum.repos.d/nginx.repo"
-	exec_cmd "echo 'enabled=1' >> /etc/yum.repos.d/nginx.repo"
+	echo '[nginx]' > /etc/yum.repos.d/nginx.repo
+	echo 'name=nginx repo' >> /etc/yum.repos.d/nginx.repo
+	echo 'baseurl=http://nginx.org/packages/centos/$releasever/$basearch/' >> /etc/yum.repos.d/nginx.repo
+	echo 'gpgcheck=0' >> /etc/yum.repos.d/nginx.repo
+	echo 'enabled=1' >> /etc/yum.repos.d/nginx.repo
 
-	print_status "Add source list mongodb."
-	exec_cmd "echo '[mongodb-org-3.2]' > /etc/yum.repos.d/mongodb-org.repo"
-	exec_cmd "echo 'name=MongoDB Repository' >> /etc/yum.repos.d/mongodb-org.repo"
-	exec_cmd "echo 'baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.2/x86_64/' >> /etc/yum.repos.d/mongodb-org.repo"
-	exec_cmd "echo 'gpgcheck=1' >> /etc/yum.repos.d/mongodb-org.repo"
-	exec_cmd "echo 'enabled=1' >> /etc/yum.repos.d/mongodb-org.repo"
-	exec_cmd "echo 'gpgkey=https://www.mongodb.org/static/pgp/server-3.2.asc' >> /etc/yum.repos.d/mongodb-org.repo"
+	echo '[mongodb-org-3.2]' > /etc/yum.repos.d/mongodb-org.repo
+	echo 'name=MongoDB Repository' >> /etc/yum.repos.d/mongodb-org.repo
+	echo 'baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.2/x86_64/' >> /etc/yum.repos.d/mongodb-org.repo
+	echo 'gpgcheck=1' >> /etc/yum.repos.d/mongodb-org.repo
+	echo 'enabled=1' >> /etc/yum.repos.d/mongodb-org.repo
+	echo 'gpgkey=https://www.mongodb.org/static/pgp/server-3.2.asc' >> /etc/yum.repos.d/mongodb-org.repo
 
-	exec_cmd "sudo yum repolist"
+	sudo yum repolist
 
-	print_status "Install nodejs version 8."
-	exec_cmd "curl --silent --location https://rpm.nodesource.com/setup_8.x | bash -"
-	exec_cmd "sudo yum install -y nodejs"
+	curl --silent --location https://rpm.nodesource.com/setup_8.x | bash -
+	sudo yum install -y nodejs nginx git mongodb-org net-tools nano
 
-	print_status "Install ngnix."
-	exec_cmd "sudo yum install -y nginx"
-	exec_cmd "sudo chkconfig nginx on"
+	sudo chkconfig nginx on
 
-	print_status "Install git."
-	exec_cmd "sudo yum install -y git"
-
-	print_status "Install database MongoDB."
-	exec_cmd "sudo yum install -y  mongodb-org"
-	exec_cmd "sudo systemctl start mongod || sudo systemctl reload mongod"
+	sudo systemctl start mongod || sudo systemctl reload mongod
 
 #fi
 # if system debian
@@ -128,74 +49,54 @@ elif [ -f /etc/lsb-release ]; then
 		fi
 	}
 
-	print_status "Add source list ngnix."
-  	exec_cmd "wget https://nginx.org/keys/nginx_signing.key -O - | sudo apt-key add - "
-  	exec_cmd "echo 'deb http://nginx.org/packages/ubuntu/ ${DISTRO} nginx' > /etc/apt/sources.list.d/nginx.list"
-  	exec_cmd "echo 'deb-src http://nginx.org/packages/ubuntu/ ${DISTRO} nginx' >> /etc/apt/sources.list.d/nginx.list"
+	wget https://nginx.org/keys/nginx_signing.key -O - | sudo apt-key add - 
+	echo 'deb http://nginx.org/packages/ubuntu/ ${DISTRO} nginx' > /etc/apt/sources.list.d/nginx.list
+	echo 'deb-src http://nginx.org/packages/ubuntu/ ${DISTRO} nginx' >> /etc/apt/sources.list.d/nginx.list
 
-  	print_status "Add source list mingodb."
-  	exec_cmd "sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927"
-  	exec_cmd "echo 'deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse' | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list"
+	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+	echo 'deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse' | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
-  	print_status "Install nodejs version 8."
-  	exec_cmd "curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -"
-  	exec_cmd "sudo apt-get install -y nodejs"
+	curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+	sudo apt-get install -y nodejs nginx git
 
-
-  	print_status "Install ngnix."
-  	exec_cmd "sudo apt-get install -y nginx"
-  	exec_cmd "sudo update-rc.d nginx defaults"
-
-  	print_status "Install git."
-  	exec_cmd "sudo apt-get install -y git"
-
-  	print_status "Install database MongoDB."
-  	exec_cmd "sudo apt-get install -y mongodb mongodb-server"
-
+	sudo update-rc.d nginx defaults
+fi
+if [ ! -d /root/.ssh ]; then
+  mkdir .ssh 
 fi
 
-exec_cmd 'cd && cd .ssh && [ -f ./id_rsa ] && echo "Rsa Key Found" || (echo "Rsa Key Not found, Create RSA Key" && ssh-keygen -t rsa -N "" -f id_rsa) && cd ~'
-
-exec_cmd 'usermod --password $(echo root | openssl passwd -1 -stdin) root'
+cd /root && cd .ssh && [ -f ./id_rsa ] && echo "Rsa Key Found" || (echo "Rsa Key Not found, Create RSA Key" && ssh-keygen -t rsa -N "" -f id_rsa) && cd ~
 
 cd
-exec_cmd 'rm -rf nginxConfigTool'
-print_status "Clone git repository NGINX Config Tool"
-exec_cmd 'git clone https://github.com/ricardoSa84/nginxConfigTool'
+rm -rf ${folderNginx}
 
-print_status "Install node models"
-exec_cmd "cd $(pwd)/nginxConfigTool/ && sudo npm install"
-exec_cmd "cd $(pwd)/nginxConfigTool/ && sudo npm install opennebula"
+git clone https://github.com/ricardoSa84/nginxConfigTool
 
-cd
-print_status "Copy necessary files to works this program."
-exec_cmd "sudo mv -f $(pwd)/nginxConfigTool/FilesMove/dashboard /etc/nginx/ || sudo cp -f $(pwd)/nginxConfigTool/FilesMove/dashboard/* /etc/nginx/dashboard/ || true"
-exec_cmd "sudo cp $(pwd)/nginxConfigTool/FilesMove/conf.d/* /etc/nginx/conf.d/ || true"
-
-print_status "Backup nginx.conf to nginx.conf.back."
-exec_cmd "sudo mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.back"
-
-print_status "Add new file nginx.conf."
-exec_cmd "sudo cp $(pwd)/nginxConfigTool/FilesMove/nginx/nginx.conf /etc/nginx/ || true"
-
-print_status "Remove Old Files."
-exec_cmd "sudo rm -rf $(pwd)/nginxConfigTool/FilesMove || true"
-
-
-print_status "Install module to run nginxConfigTool."
-exec_cmd "sudo npm install pm2 -g"
-
-print_status "Create Collector File."
-exec_cmd "echo 'module.exports = { > $(pwd)/nginxConfigTool/collectorServer.js"
-exec_cmd "echo '    collectorServer: \"[IPSTATION]\" >> $(pwd)/nginxConfigTool/collectorServer.js"
-exec_cmd "echo '}' >> $(pwd)/nginxConfigTool/collectorServer.js"
+cd ${folderNginx} && sudo npm install
+cd ${folderNginx} && sudo npm install opennebula
 
 cd
-exec_cmd "cd $(pwd)/nginxConfigTool/ && sudo pm2 start startcollector.js --name 'nginxConfigToolCollector' || true"
-exec_cmd "sudo pm2 save || true"
-exec_cmd "sudo pm2 startup systemd || true"
 
-print_status "Restart system services."
-exec_cmd "sudo service nginx restart || sudo service nginx start"
+sudo mv -f ${folderNginx}/FilesMove/dashboard /etc/nginx/ || sudo cp -f ${folderNginx}/FilesMove/dashboard/* /etc/nginx/dashboard/ || true
+sudo cp ${folderNginx}/FilesMove/conf.d/* /etc/nginx/conf.d/ || true
+
+sudo mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.back
+
+sudo cp ${folderNginx}/FilesMove/nginx/nginx.conf /etc/nginx/ || true
+
+sudo rm -rf ${folderNginx}/FilesMove || true
+
+sudo npm install pm2 -g
+
+echo 'module.exports = {' > ${folderNginx}/collectorServer.js
+echo '    collectorServer: "[IPSTATION]"' >> ${folderNginx}/collectorServer.js
+echo '}' >> $(pwd)/nginxConfigTool/collectorServer.js
+
+cd
+cd ${folderNginx}/ && sudo pm2 start startcollector.js --name 'nginxConfigToolCollector' || true
+sudo pm2 save || true
+sudo pm2 startup systemd || true
+
+sudo service nginx restart || sudo service nginx start
 
 exit 0
