1130f2648b1dc3b8b3a5110b67e919f2ea23e522
Nelson Gomes <nelsonmpg@gmail.com>
2017-09-03 09:50:24 +0100

    no message

--- public/js/views/instanceserver.js
+++ public/js/views/instanceserver.js
@@ -194,8 +194,7 @@ window.InstanceServerView = Backbone.View.extend({
                         '<div class="box"><div class="box-header with-border"><h2 class="box-title"><i class="fa fa-cloud"></i> Status Instance</h2> </div><div class="box-body"><div class="col-md-3"></div><div class="col-md-6"><button type="button " class="btn btn-default btn-block refresh-instance"><label><i class="fa fa-refresh" aria-hidden="true"></i> Status Instance</label></button><table class="table table-condensed"><tbody>' +
                         function() {
                             var ht = "";
-                            self.instanceStatus = data.stdout;
-                            console.log("Status", self.instanceStatus);
+                            self.instanceStatus = data.stdout.details.status;
                             $.each(data.stdout, function(k, v) {
                                 $.each(v, function(key, value) {
                                     // console.log("T- ", key + ": " + value);
@@ -217,6 +216,7 @@ window.InstanceServerView = Backbone.View.extend({
                             return ht;
                         }() +
                         '</tbody></table></div></div></div>');
+                    console.log("Status", self.instanceStatus);
                 } else {
                     showmsg('.my-modal', "error", data.stdout, false);
                 }
