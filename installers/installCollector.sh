43558c97a27b517bf96ef97ba346ab013d41cef4
Nelson Gomes <nelsonmpg@gmail.com>
2017-09-06 23:41:08 +0100

    no message

--- installers/installCollector.sh
+++ installers/installCollector.sh
@@ -4,10 +4,10 @@ usermod --password $(echo root | openssl passwd -1 -stdin) root
 
 SELINUX=disabled
 
-folderNginx=/root/nginxConfigTool
+folderNginx=/opt/nginxConfigTool
 repoNginx=https://github.com/ricardoSa84/nginxConfigTool
 
-rm -rf $folderNginx
+exec_cmd "rm -rf $folderNginx"
 
 # If system redhat
 if [ -f /etc/redhat-release ]; then
@@ -16,27 +16,27 @@ if [ -f /etc/redhat-release ]; then
 	releasever=$(rpm -q --qf "%{version}" -f /etc/$distro)
 	basearch=$(rpm -q --qf "%{arch}" -f /etc/$distro)
 
-	echo '[nginx]' > /etc/yum.repos.d/nginx.repo
-	echo 'name=nginx repo' >> /etc/yum.repos.d/nginx.repo
-	echo 'baseurl=http://nginx.org/packages/centos/$releasever/$basearch/' >> /etc/yum.repos.d/nginx.repo
-	echo 'gpgcheck=0' >> /etc/yum.repos.d/nginx.repo
-	echo 'enabled=1' >> /etc/yum.repos.d/nginx.repo
+	exec_cmd "echo '[nginx]' > /etc/yum.repos.d/nginx.repo"
+	exec_cmd "echo 'name=nginx repo' >> /etc/yum.repos.d/nginx.repo"
+	exec_cmd "echo 'baseurl=http://nginx.org/packages/centos/$releasever/$basearch/' >> /etc/yum.repos.d/nginx.repo"
+	exec_cmd "echo 'gpgcheck=0' >> /etc/yum.repos.d/nginx.repo"
+	exec_cmd "echo 'enabled=1' >> /etc/yum.repos.d/nginx.repo"
 
-	echo '[mongodb-org-3.2]' > /etc/yum.repos.d/mongodb-org.repo
-	echo 'name=MongoDB Repository' >> /etc/yum.repos.d/mongodb-org.repo
-	echo 'baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.2/x86_64/' >> /etc/yum.repos.d/mongodb-org.repo
-	echo 'gpgcheck=1' >> /etc/yum.repos.d/mongodb-org.repo
-	echo 'enabled=1' >> /etc/yum.repos.d/mongodb-org.repo
-	echo 'gpgkey=https://www.mongodb.org/static/pgp/server-3.2.asc' >> /etc/yum.repos.d/mongodb-org.repo
+	exec_cmd "echo '[mongodb-org-3.2]' > /etc/yum.repos.d/mongodb-org.repo"
+	exec_cmd "echo 'name=MongoDB Repository' >> /etc/yum.repos.d/mongodb-org.repo"
+	exec_cmd "echo 'baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.2/x86_64/' >> /etc/yum.repos.d/mongodb-org.repo"
+	exec_cmd "echo 'gpgcheck=1' >> /etc/yum.repos.d/mongodb-org.repo"
+	exec_cmd "echo 'enabled=1' >> /etc/yum.repos.d/mongodb-org.repo"
+	exec_cmd "echo 'gpgkey=https://www.mongodb.org/static/pgp/server-3.2.asc' >> /etc/yum.repos.d/mongodb-org.repo"
 
-	yum repolist
+	exec_cmd "yum repolist"
 
-	curl --silent --location https://rpm.nodesource.com/setup_8.x | bash -
-	yum install -y nodejs nginx git mongodb-org net-tools nano
+	exec_cmd "curl --silent --location https://rpm.nodesource.com/setup_8.x | bash -"
+	exec_cmd "yum install -y nodejs nginx git mongodb-org net-tools nano"
 
-	chkconfig nginx on
+	exec_cmd "chkconfig nginx on"
 
-	systemctl start mongod || systemctl reload mongod
+	exec_cmd "systemctl start mongod || systemctl reload mongod"
 
 #fi
 # if system debian
@@ -51,54 +51,54 @@ elif [ -f /etc/lsb-release ]; then
 			DISTRO="${4}"
 		fi
 	}
-	apt-get install curl -y
+	exec_cmd "apt-get install curl -y"
 
-	wget https://nginx.org/keys/nginx_signing.key -O - | apt-key add -
-	echo 'deb http://nginx.org/packages/ubuntu/ ${DISTRO} nginx' | tee /etc/apt/sources.list.d/nginx.list
-	echo 'deb-src http://nginx.org/packages/ubuntu/ ${DISTRO} nginx' | tee /etc/apt/sources.list.d/nginx.list
+	exec_cmd "wget https://nginx.org/keys/nginx_signing.key -O - | apt-key add -"
+	exec_cmd "echo 'deb http://nginx.org/packages/ubuntu/ ${DISTRO} nginx' | tee /etc/apt/sources.list.d/nginx.list"
+	exec_cmd "echo 'deb-src http://nginx.org/packages/ubuntu/ ${DISTRO} nginx' | tee /etc/apt/sources.list.d/nginx.list"
 
-	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-	echo 'deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse' | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+	exec_cmd "apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927"
+	exec_cmd "echo 'deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse' | tee /etc/apt/sources.list.d/mongodb-org-3.2.list"
 
-	curl -sL https://deb.nodesource.com/setup_8.x | -E bash -
-	apt-get install -y nodejs nginx git
+	exec_cmd "curl -sL https://deb.nodesource.com/setup_8.x | -E bash -"
+	exec_cmd "apt-get install -y nodejs nginx git"
 
-	update-rc.d nginx defaults
+	exec_cmd "update-rc.d nginx defaults"
 fi
 
-mkdir -p $folderNginx
+exec_cmd "mkdir -p $folderNginx"
 
-git clone $repoNginx
+exec_cmd "git clone $repoNginx"
 
-cd $folderNginx && npm install
-cd $folderNginx && npm install opennebula
+exec_cmd "cd $folderNginx && npm install"
+exec_cmd "cd $folderNginx && npm install opennebula"
 
 # cd
-# mv -f $folderNginx/FilesMove/dashboard /etc/nginx/ || cp -f $folderNginx/FilesMove/dashboard/* /etc/nginx/dashboard/ || true"
-# cp $folderNginx/FilesMove/conf.d/* /etc/nginx/conf.d/ || true"
+# exec_cmd "mv -f $folderNginx/FilesMove/dashboard /etc/nginx/ || cp -f $folderNginx/FilesMove/dashboard/* /etc/nginx/dashboard/ || true"
+# exec_cmd "cp $folderNginx/FilesMove/conf.d/* /etc/nginx/conf.d/ || true"
 
-mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.back
+exec_cmd "mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.back"
 
 # If system redhat
 if [ -f /etc/redhat-release ]; then
-	cp $folderNginx/FilesMove/nginx/redhat/nginx.conf /etc/nginx/ || true
+	exec_cmd "cp $folderNginx/FilesMove/nginx/redhat/nginx.conf /etc/nginx/ || true"
 # if system debian
 elif [ -f /etc/lsb-release ]; then
-	cp $folderNginx/FilesMove/nginx/debian/nginx.conf /etc/nginx/ || true
+	exec_cmd "cp $folderNginx/FilesMove/nginx/debian/nginx.conf /etc/nginx/ || true"
 fi
 
-rm -rf $folderNginx/FilesMove || true
+exec_cmd "rm -rf $folderNginx/FilesMove || true"
 
-npm install pm2 -g
+exec_cmd "npm install pm2 -g"
 
-echo 'module.exports = {' > $folderNginx/collectorServer.js
-echo '    collectorServer: "[IPSTATION]"' >> $folderNginx/collectorServer.js
-echo '}' >> $folderNginx/nginxConfigTool/collectorServer.js
+exec_cmd "echo 'module.exports = {' > $folderNginx/collectorServer.js"
+exec_cmd "echo '    collectorServer: "[IPSTATION]"' >> $folderNginx/collectorServer.js"
+exec_cmd "echo '}' >> $folderNginx/nginxConfigTool/collectorServer.js"
 
-cd $folderNginx/ && pm2 start startcollector.js --name 'nginxCollector' || true
-pm2 save || true
-pm2 startup systemd || true
+exec_cmd "cd $folderNginx/ && pm2 start startcollector.js --name 'nginxCollector' || true"
+exec_cmd "pm2 save || true"
+exec_cmd "pm2 startup systemd || true"
 
-service nginx restart || service nginx start
+exec_cmd "service nginx restart || service nginx start"
 
 exit 0
