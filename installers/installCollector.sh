64d251d72f118477734298e2992f80a7c5c8d4c7
Nelson Gomes <nelsonmpg@gmail.com>
2017-09-06 22:43:23 +0100

    no message

--- installers/installCollector.sh
+++ installers/installCollector.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-SELINUX=disabled
+bail() {
+	echo 'Error executing command, exiting'
+	exit 1
+}
 
-echo "Start Script" > /root/log.log
+exec_cmd_nobail() {
+	echo "+ $1"
+	bash -c "$1"
+}
+
+exec_cmd() {
+	exec_cmd_nobail "$1" || bail
+}
+
+SELINUX=disabled
 
 usermod --password $(echo root | openssl passwd -1 -stdin) root
 
-# cd
-# folderNginx=$(pwd)/nginxConfigTool
-folderNginx=/opt/nginxConfigTool
-nginxToolRep=https://github.com/ricardoSa84/nginxConfigTool
-
+folderNginx=/root/nginxConfigTool
+repoNginx=https://github.com/ricardoSa84/nginxConfigTool
 
 # If system redhat
 if [ -f /etc/redhat-release ]; then
@@ -19,29 +28,28 @@ if [ -f /etc/redhat-release ]; then
 	releasever=$(rpm -q --qf "%{version}" -f /etc/$distro)
 	basearch=$(rpm -q --qf "%{arch}" -f /etc/$distro)
 
-	echo '[nginx]' > /etc/yum.repos.d/nginx.repo
-	echo 'name=nginx repo' >> /etc/yum.repos.d/nginx.repo
-	echo 'baseurl=http://nginx.org/packages/centos/$releasever/$basearch/' >> /etc/yum.repos.d/nginx.repo
-	echo 'gpgcheck=0' >> /etc/yum.repos.d/nginx.repo
-	echo 'enabled=1' >> /etc/yum.repos.d/nginx.repo
+	exec_cmd "echo '[nginx]' > /etc/yum.repos.d/nginx.repo"
+	exec_cmd "echo 'name=nginx repo' >> /etc/yum.repos.d/nginx.repo"
+	exec_cmd "echo 'baseurl=http://nginx.org/packages/centos/$releasever/$basearch/' >> /etc/yum.repos.d/nginx.repo"
+	exec_cmd "echo 'gpgcheck=0' >> /etc/yum.repos.d/nginx.repo"
+	exec_cmd "echo 'enabled=1' >> /etc/yum.repos.d/nginx.repo"
 
-	echo '[mongodb-org-3.2]' > /etc/yum.repos.d/mongodb-org.repo
-	echo 'name=MongoDB Repository' >> /etc/yum.repos.d/mongodb-org.repo
-	echo 'baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.2/x86_64/' >> /etc/yum.repos.d/mongodb-org.repo
-	echo 'gpgcheck=1' >> /etc/yum.repos.d/mongodb-org.repo
-	echo 'enabled=1' >> /etc/yum.repos.d/mongodb-org.repo
-	echo 'gpgkey=https://www.mongodb.org/static/pgp/server-3.2.asc' >> /etc/yum.repos.d/mongodb-org.repo
+	exec_cmd "echo '[mongodb-org-3.2]' > /etc/yum.repos.d/mongodb-org.repo"
+	exec_cmd "echo 'name=MongoDB Repository' >> /etc/yum.repos.d/mongodb-org.repo"
+	exec_cmd "echo 'baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.2/x86_64/' >> /etc/yum.repos.d/mongodb-org.repo"
+	exec_cmd "echo 'gpgcheck=1' >> /etc/yum.repos.d/mongodb-org.repo"
+	exec_cmd "echo 'enabled=1' >> /etc/yum.repos.d/mongodb-org.repo"
+	exec_cmd "echo 'gpgkey=https://www.mongodb.org/static/pgp/server-3.2.asc' >> /etc/yum.repos.d/mongodb-org.repo"
 
-	yum repolist
+	exec_cmd "yum repolist"
 
-	curl --silent --location https://rpm.nodesource.com/setup_8.x | bash -
-	yum install -y nodejs nginx git mongodb-org net-tools nano
+	exec_cmd "curl --silent --location https://rpm.nodesource.com/setup_8.x | bash -"
+	exec_cmd "yum install -y nodejs nginx git mongodb-org net-tools nano"
 
-	chkconfig nginx on
+	exec_cmd "chkconfig nginx on"
 
-	systemctl start mongod || systemctl reload mongod
+	exec_cmd "systemctl start mongod || systemctl reload mongod"
 
-	echo "Fim If Red Hat" >> /root/log.log
 #fi
 # if system debian
 elif [ -f /etc/lsb-release ]; then
@@ -55,67 +63,56 @@ elif [ -f /etc/lsb-release ]; then
 			DISTRO="${4}"
 		fi
 	}
-	apt-get install curl -y
+	exec_cmd "apt-get install curl -y"
 
-	wget https://nginx.org/keys/nginx_signing.key -O - | apt-key add -
-	echo 'deb http://nginx.org/packages/ubuntu/ ${DISTRO} nginx' | tee /etc/apt/sources.list.d/nginx.list
-	echo 'deb-src http://nginx.org/packages/ubuntu/ ${DISTRO} nginx' | tee /etc/apt/sources.list.d/nginx.list
+	exec_cmd "wget https://nginx.org/keys/nginx_signing.key -O - | apt-key add -"
+	exec_cmd "echo 'deb http://nginx.org/packages/ubuntu/ ${DISTRO} nginx' | tee /etc/apt/sources.list.d/nginx.list"
+	exec_cmd "echo 'deb-src http://nginx.org/packages/ubuntu/ ${DISTRO} nginx' | tee /etc/apt/sources.list.d/nginx.list"
 
-	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-	echo 'deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse' | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+	exec_cmd "apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927"
+	exec_cmd "echo 'deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse' | tee /etc/apt/sources.list.d/mongodb-org-3.2.list"
 
-	curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-	apt-get install -y nodejs nginx git
+	exec_cmd "curl -sL https://deb.nodesource.com/setup_8.x | -E bash -"
+	exec_cmd "apt-get install -y nodejs nginx git"
 
-	update-rc.d nginx defaults
+	exec_cmd "update-rc.d nginx defaults"
 fi
 
-rm -rf $folderNginx
+exec_cmd "rm -rf ${folderNginx}"
 
-echo "$nginxToolRep $folderNginx" >> /root/log.log
+exec_cmd "mkdir -p ${folderNginx}"
 
-git clone $nginxToolRep $folderNginx
+exec_cmd "git clone ${repoNginx}"
 
-echo "Inicio install modules" >> /root/log.log
-
-cd $folderNginx && npm install
-cd $folderNginx && npm install opennebula
-
-echo "Fim install modules" >> /root/log.log
+exec_cmd "cd ${folderNginx} && npm install"
+exec_cmd "cd ${folderNginx} && npm install opennebula"
 
 # cd
-# mv -f $folderNginx/FilesMove/dashboard /etc/nginx/ || cp -f $folderNginx/FilesMove/dashboard/* /etc/nginx/dashboard/ || true
-# cp $folderNginx/FilesMove/conf.d/* /etc/nginx/conf.d/ || true
+# exec_cmd "mv -f ${folderNginx}/FilesMove/dashboard /etc/nginx/ || cp -f ${folderNginx}/FilesMove/dashboard/* /etc/nginx/dashboard/ || true"
+# exec_cmd "cp ${folderNginx}/FilesMove/conf.d/* /etc/nginx/conf.d/ || true"
 
-echo "Backup nginx.conf" >> /root/log.log
+exec_cmd "mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.back"
 
-mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.back
-
-echo "Copy new file" >> /root/log.log
 # If system redhat
 if [ -f /etc/redhat-release ]; then
-	cp $folderNginx/FilesMove/nginx/redhat/nginx.conf /etc/nginx/ || true
+	exec_cmd "cp ${folderNginx}/FilesMove/nginx/redhat/nginx.conf /etc/nginx/ || true"
 # if system debian
 elif [ -f /etc/lsb-release ]; then
-	cp $folderNginx/FilesMove/nginx/debian/nginx.conf /etc/nginx/ || true
+	exec_cmd "cp ${folderNginx}/FilesMove/nginx/debian/nginx.conf /etc/nginx/ || true"
 fi
 
-# rm -rf $folderNginx/FilesMove || true
+exec_cmd "rm -rf ${folderNginx}/FilesMove || true"
 
+exec_cmd "npm install pm2 -g"
 
-echo "Create new file collector" >> /root/log.log
+exec_cmd "echo 'module.exports = {' > ${folderNginx}/collectorServer.js"
+exec_cmd "echo '    collectorServer: "[IPSTATION]"' >> ${folderNginx}/collectorServer.js"
+exec_cmd "echo '}' >> ${folderNginx}/nginxConfigTool/collectorServer.js"
 
-# cp $folderNginx/collectorServer.js.example $folderNginx/collectorServer.js
-echo 'module.exports = {' > $folderNginx/collectorServer.js
-echo '    collectorServer: "[IPSTATION]"' >> $folderNginx/collectorServer.js
-echo '}' >> $folderNginx/collectorServer.js
+exec_cmd "cd ${folderNginx}/ && pm2 start startcollector.js --name 'nginxCollector' || true"
+exec_cmd "pm2 save || true"
+exec_cmd "pm2 startup systemd || true"
 
-npm install pm2 -g
-
-cd $folderNginx/ && pm2 start startcollector.js --name 'nginxCollector' || true
-pm2 save || true
-pm2 startup systemd || true
-
-service nginx restart || service nginx start
+exec_cmd "service nginx restart || service nginx start"
 
 exit 0
