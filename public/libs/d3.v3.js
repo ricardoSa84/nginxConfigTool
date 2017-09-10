c1387d3bab515d6a75970eef463195fbce79cd55
Nelson Gomes <nelsonmpg@gmail.com>
2017-09-10 15:46:30 +0100

    no message

--- public/libs/d3.v3.js
+++ public/libs/d3.v3.js
@@ -3775,7 +3775,7 @@ d3 = function() {
         function e(n, e) {
             var r = t.call(this, n, e, this.getAttribute(u));
             return r && function(n) {
-            	console.log(u, n, r(n));
+            	console.log("A", u, "B",  n, "C", r, "D", r(n));
                 this.setAttribute(u, r(n))
             }
         }
