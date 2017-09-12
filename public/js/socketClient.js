1807adfd04dbeff8983487c0fd5c4a6281029dd2
Nelson Gomes <nelsonmpg@gmail.com>
2017-09-12 21:05:43 +0100

    no message

--- public/js/socketClient.js
+++ public/js/socketClient.js
@@ -1,48 +1,50 @@
-var socketClient = function (options) {
+var socketClient = function(options) {
     var self = this;
     self.vent = options.vent;
 
     self.hostname = window.location.host.split("#")[0];
 
-    self.connect = function () {
+    self.connect = function() {
         self.socket = io.connect(self.hostname);
         self.setResponseListeners(self.socket);
     };
 
-    self.disconnect = function () {
+    self.disconnect = function() {
         if (self.socket) {
             self.socket.io.close();
         }
     };
 
-    self.setcommand = function (command) {
+    self.setcommand = function(command) {
         self.socket.emit('stdin', command);
     };
-    
-    self.setResponseListeners = function (socket) {
 
-        socket.on('stdout', function (data) {
+    self.setResponseListeners = function(socket) {
+
+        socket.on('stdout', function(data) {
             self.vent.trigger('stdout', data);
         });
-        socket.on('stderr', function (data) {
+        socket.on('stderr', function(data) {
             self.vent.trigger('stderr', data);
         });
-        socket.on('disconnect', function () {
+        socket.on('disconnect', function() {
             self.vent.trigger('disconnect');
         });
-        socket.on('enable', function () {
+        socket.on('enable', function() {
             self.vent.trigger('enable');
         });
-        socket.on('disable', function () {
+        socket.on('disable', function() {
             self.vent.trigger('disable');
         });
 
-        socket.on('dataNewServer', function(data){
+        socket.on('dataNewServer', function(data) {
             var dispMsg = data;
-            console.log("Teste", $(document).find(".my-modal").css("display"));
             if ($(document).find(".my-modal").css("display") == "block") {
                 console.log("dentro");
-                dispMsg += $(document).find(".my-modal").children().children().children().children().find("h4");
+                dispMsg += $(document).find(".my-modal").children().children().children().children().find("h4").each(function() {
+                    console.log("Text", this.text);
+                    return this.text;
+                });
             }
             showmsg('.my-modal', "success", dispMsg, false);
         });
